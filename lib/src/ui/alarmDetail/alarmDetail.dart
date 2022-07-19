ac989687c2cf74de6ebbf1a3f4fe5de21622edf7
nakisara01 <nakisara01@ajou.ac.kr>
2022-07-19 14:03:15 +0900

    test

--- lib/src/ui/alarmDetail/alarmDetail.dart
+++ lib/src/ui/alarmDetail/alarmDetail.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class AlarmDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Detail")));
+    return Scaffold(body: Center(child: Text("DetailPage")));
   }
 }
